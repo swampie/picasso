5e1f1cc0271901d3db1d63f3e39daddc15482a02
swampie <matteo.fiandesio@gmail.com>
2011-05-20 15:04:13 +0200

    a

---
+++ modules/avatar-module.js
@@ -0,0 +1,15 @@
+exports = module.exports = AvatarModule;
+
+function AvatarModule(){}
+
+exports.conf =function(){
+    return new Object({
+	name: 'AvatarModule', 
+	registeredOption :"-a",
+	description : "module to manage user avatars"
+    });
+}
+
+exports.exec = function{
+    console.log("executing module" + this.conf().name);
+}
