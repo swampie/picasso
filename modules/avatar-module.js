c0141d7f9db9c85f40904b88e895bc9626762a70
swampie <matteo.fiandesio@gmail.com>
2011-05-23 18:32:12 +0200

    blah blah

--- modules/avatar-module.js
+++ modules/avatar-module.js
@@ -1,15 +1,21 @@
 exports = module.exports = AvatarModule;
-
+var im = require('imagemagick');
 function AvatarModule(){}
 
 exports.conf =function(){
     return new Object({
 	name: 'AvatarModule', 
 	registeredOption :"-a",
-	description : "module to manage user avatars"
+	description : "module to manage user avatars",
+	suffixes = {'pq':{width:19,height:19},
+                    'md':{width:30,height:30},
+		    'gd':{width:48,height:48},
+                    'xg':{width:70,height:70}
+                    }
     });
 }
 
-exports.exec = function(){
+exports.exec = function(args){
     console.log("executing module" + this.conf().name);
+    im.composite();
 }
