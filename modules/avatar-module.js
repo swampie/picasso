6ae702f284fa79a27b4d09165c51bbcb66c85ef9
swampie <matteo.fiandesio@gmail.com>
2011-05-24 13:53:50 +0200

    first composite test,needs refactor

--- modules/avatar-module.js
+++ modules/avatar-module.js
@@ -1,4 +1,6 @@
 exports = module.exports = AvatarModule;
+var Step = require('step');
+var path = require('path');
 var im = require('imagemagick');
 function AvatarModule(){}
 
@@ -7,15 +9,52 @@ exports.conf =function(){
 	name: 'AvatarModule', 
 	registeredOption :"-a",
 	description : "module to manage user avatars",
-	suffixes = {'pq':{width:19,height:19},
-                    'md':{width:30,height:30},
-		    'gd':{width:48,height:48},
-                    'xg':{width:70,height:70}
-                    }
+	suffixes : [{suffix:'pq',width:19,height:19},
+                    {suffix:'md',width:30,height:30},
+		    {suffix:'gd',width:48,height:48},
+                    {suffix:'xg',width:70,height:70}
+                    ]
     });
 }
 
 exports.exec = function(args){
-    console.log("executing module" + this.conf().name);
-    im.composite();
+    var self = this;
+    var destination = path.dirname(args.input);
+    console.log(args.input);
+    Step(
+	function startLog(){
+	    console.log("executing module" + self.conf().name+" for "+args.input);
+	    return this;
+        },
+	function converting(){
+	    self.conf().suffixes.forEach(
+		function(it){
+			console.log("converting to "+it.suffix);
+			im.convert([args.input,
+				   "-colorspace",
+				   "RGB",
+				   "-type",
+				   "TrueColor",
+				   "-identify",
+				   "-strip",
+                                   "-resize",
+                                   it.width/2,
+				   "-resize",
+				   it.width/2+"x<",
+				   "-resize",
+				   "50%",
+                                   "-gravity",
+                                   "center",
+                                   "-crop",it.width+"x"+it.height+"+0+0",
+                                   destination+"/"+it.suffix+"_"+path.basename(args.input,path.extname(args.input)+".png")],function(err,stdout,stderr){
+				if(err){
+					console.log(err);	
+				}else{
+					console.log(stdout);
+				}
+			})
+		});
+	}
+	
+    );
 }
---
+++ test/avatarTest.js
@@ -0,0 +1,3 @@
+var a = require('../modules/avatar-module');
+var ags = {input:'/home/mfiandesio/nodejs/picasso/test/avatartest.jpg'};
+a.exec(ags);
